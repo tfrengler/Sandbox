dacb0a4f086ac283be8dbca8e2c34ea67129ba68
Thomas Frengler <thomasfrengler@gmail.com>
2021-07-24 10:46:55 +0200

    WIP stuff

--- WIP/test.c
+++ WIP/test.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+typedef unsigned char byte;
+typedef unsigned short ushort;
+typedef unsigned int uint;
+typedef unsigned long ulong;
+
 typedef struct
 {
     unsigned int MyInt;
